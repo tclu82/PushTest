22b1c0087018c2ef536913eb8b4407a329a505a4
Zac Lu <tclu82@uw.edu>
2016-05-12 16:40:18 -0700

    20160512

---
+++ app/src/main/java/com/zaclu0601/pushtest/MainActivity.java
@@ -0,0 +1,19 @@
+package com.zaclu0601.pushtest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    private void testPush() {
+        int i;
+        i = 10;
+    }
+
+}
